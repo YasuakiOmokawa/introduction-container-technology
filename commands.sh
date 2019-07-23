77bde8d48e07f1ffb4e7856ac7cca703549b5a83
面川泰明 <omokawa@senk-inc.co.jp>
2019-07-23 13:56:12 +0900

    Update commands.sh

--- commands.sh
+++ commands.sh
@@ -1,9 +1,15 @@
+## コンテナのルートファイルシステムを作成
+## Dockerのbashイメージをテンポラリディレクトリに展開し、ここをコンテナのルートファイルシステムとする
 
-# コンテナのルートファイルシステムを作成
-# Dockerのbashイメージをテンポラリディレクトリに展開し、ここをコンテナのルートファイルシステムとする
+# 一時ディレクトリの作成
 ROOTFS=$(mktemp -d)
+
+# Dockerイメージの展開
 CID=$(sudo docker container create bash)
+
+# 
 sudo docker container export $CID | tar -x -C $ROOTFS
+
 ln -s /usr/local/bin/bash $ROOTFS/bin/bash
 sudo docker container rm $CID
 
