ad1a6655c68fd3b71a5f3a918a0891530f91f342
面川泰明 <omokawa@senk-inc.co.jp>
2019-07-28 20:59:39 +0900

    cgroupの割り当ての説明を追記

--- commands.sh
+++ commands.sh
@@ -28,9 +28,19 @@ UUID=$(uuidgen)
 # 書式）　cgcreate -t uid:gid -a uid:gid -g subsystems:path
 # -t は、タスクを追加できるオプション。ユーザidとグループidを指定して、このグループのtasks疑似ファイルを所有する。
 # -a は、タスクが持つシステムリソースへのアクセスを変更できるオプション。ユーザidとグループidを指定して、このグループのtasks以外の全疑似ファイルを所有する。
-# -g は、コントロールグループ
+# -g は、コントロールグループが作成される「階層」を、subsystemという概念に関連付ける。cpuはCPU,memoryはメモリのこと。
+#   コロンの後にはグループ名を指定する。今回は上記で作成したUUIDをグループ名とする。このコントロールグループはUUIDの名前で2つ作成され、
+#   それぞれcpuとmemoryサブシステムによって制御される。
 sudo cgcreate -t $(id -un):$(id -gn) -a $(id -un):$(id -gn) -g cpu,memory:$UUID
 
+# サブシステムのパラメータを設定。
+# 書式）　cgset -r parameter=value path_to_cgroup
+#   parameter は、サブシステムの値を含んだ擬似ファイル
+# UUIDで指定したコントロールグループに対し、memoryサブシステムのlimit_in_bytesを10MBに指定
 cgset -r memory.limit_in_bytes=10000000 $UUID
+
+# UUIDで指定したコントロールグループに対し、cpuサブシステムのcfs_period_usを1秒に指定（割り当ての単位はマイクロ秒。ここでは上限いっぱい割り当てる）
 cgset -r cpu.cfs_period_us=1000000 $UUID
+
+# UUIDで指定したコントロールグループに対し、cpuサブシステムのcfs_quota_usを300usに指定（割り当ての単位はマイクロ秒。ここでは上記の制限値の30%を割り当てる）
 cgset -r cpu.cfs_quota_us=300000 $UUID
